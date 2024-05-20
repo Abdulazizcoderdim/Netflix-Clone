6953952de33f13a08f0fb839aa84303f991e5530
AbdulazizRus <coderdim@gmail.com>
2024-05-20 20:04:59 +0500

    manage page

--- components/shared/manage-account.tsx
+++ components/shared/manage-account.tsx
@@ -1,7 +1,61 @@
+'use client'
+
+import { LockKeyhole, Trash2 } from 'lucide-react'
+import Image from 'next/image'
+import { useState } from 'react'
 
 const ManageAccount = () => {
+  const [isDelete, setIsDelete] = useState<boolean>(
+    JSON.parse(localStorage.getItem('isDelete') || 'false')
+  )
+  const [open, setOpen] = useState(false)
+  
+  const handleChange = () => {
+    setIsDelete(prev=>!prev)
+    localStorage.setItem('isDelete', JSON.stringify(!isDelete))
+  }
+  
   return (
-    <div>ManageAccount</div>
+    <div className="min-h-screen flex justify-center flex-col items-center relative">
+      <div className="flex justify-center flex-col items-center">
+        <h1 className="text-white font-bold text-5xl my-12">
+          Who&apos;s Watching?
+        </h1>
+
+        <ul className="flex p-0 my-12">
+          <li className="max-w-[200px] w-[155px] cursor-pointer flex flex-col items-center gap-3 min-w-[200px]">
+            <div className="relative">
+              <div className="max-w-[200px] rounded min-w-[84px] max-h-[200px] min-h-[84px] object-cover w-[155px] h-[155px] relative">
+                <Image
+                  src={
+                    'https://occ-0-2611-3663.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABfNXUMVXGhnCZwPI1SghnGpmUgqS_J-owMff-jig42xPF7vozQS1ge5xTgPTzH7ttfNYQXnsYs4vrMBaadh4E6RTJMVepojWqOXx.png?r=1d4'
+                  }
+                  alt={'account'}
+                  fill
+                />
+              </div>
+              {isDelete ? (
+                <div className="absolute transform bottom-0 z-10 contain-paint">
+                  <Trash2 className="w-8 h-8 text-red-600" />
+                </div>
+              ) : null}
+            </div>
+            <div className="flex items-center gap-1">
+              <span className="font-mono font-bold text-xl">Abdulaziz</span>
+              <LockKeyhole />
+            </div>
+          </li>
+          <li className='borde bg-[#e5b109] font-bold text-xl border-black max-w-[200px] rounded min-w-[84px] max-h-[200px] min-h-[84px] w-[155px] h-[155px] cursor-pointer flex justify-center items-center'>
+             Add account
+          </li>
+        </ul>
+        <div
+         onClick={handleChange}
+         className='bg-transparent rounded-none hover:bg-transparent !text-white border border-gray-100 cursor-pointer tracking-wide inline-flex text-sm px-[1.5rem] py-[0.5rem]'>
+          Manage Profiles
+        </div>
+      </div>
+    </div>
   )
 }
 
