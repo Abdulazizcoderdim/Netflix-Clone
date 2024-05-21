3868efe500918626c38eafbdf8771738acae3cb0
AbdulazizRus <coderdim@gmail.com>
2024-05-21 17:45:12 +0500

    mogo db connected

---
+++ lib/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose, { ConnectOptions } from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDB = async () => {
+    mongoose.set("strictQuery", true);
+
+    if(!process.env.MONGODB_URL) {
+        throw new Error("DBga ulanmadiiiiii");
+    }
+
+    if(isConnected) {
+        return;
+    }
+
+    try {
+        const options: ConnectOptions = {
+            dbName: "netflix",
+            autoIndex: true,
+        }
+
+        await mongoose.connect(process.env.MONGODB_URL, options);
+        
+        isConnected = true;
+        console.log("MongoDB ulandi ✅");
+    } catch (error) {
+        console.log('DBga ulanmadiiiiii');
+    }
+}
